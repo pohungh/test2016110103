5436717024686b1a045cc20424e3924c9e7d2ee5
pohung <kof96302@yahoo.com.tw>
2016-11-01 20:12:25 +0800

    Dialog2 editText

--- app/src/main/java/com/example/yvtc/test2016110103/MainActivity.java
+++ app/src/main/java/com/example/yvtc/test2016110103/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,22 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-        public void click1(View v)
-        {
+        public void click1(View v) {
             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
             builder.setTitle("對話框測試");
-            builder.setMessage("這是對話框內容");
+            builder.setMessage("這是對話框內容\n第二行");
 
-            builder.setPositiveButton("確定", new DialogInterface.OnClickListener(){
+            builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
 
-                public void onClick(DialogInterface dialog,int which){
+                public void onClick(DialogInterface dialog, int which) {
                     Toast.makeText(MainActivity.this, "確定被按下", Toast.LENGTH_SHORT).show();
                 }
             });
 
-            builder.setNegativeButton("取消", new DialogInterface.OnClickListener(){
+            builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
 
-                public void onClick(DialogInterface dialog,int which){
+                public void onClick(DialogInterface dialog, int which) {
                     Toast.makeText(MainActivity.this, "取消被按下", Toast.LENGTH_SHORT).show();
                 }
             });
@@ -38,6 +38,21 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     Toast.makeText(MainActivity.this, "忽略被按下", Toast.LENGTH_SHORT).show();
+                }
+            });
+            builder.show();
+        }
+        public void click2(View v)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("輸入對話框測試");
+            builder.setMessage("請輸入你的暱稱");
+            EditText ed = new EditText(MainActivity.this);
+            builder.setView(ed);
+            builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
                 }
             });
             builder.show();
