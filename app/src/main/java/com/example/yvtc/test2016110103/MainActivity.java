cc6e7a209cb455a385c5b1579c14b927156760bb
pohung <kof96302@yahoo.com.tw>
2016-11-02 18:54:21 +0800

    clone.choice

--- app/src/main/java/com/example/yvtc/test2016110103/MainActivity.java
+++ app/src/main/java/com/example/yvtc/test2016110103/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     int ch = -1;
     int tmp = -1;
     boolean b[] = new boolean[4];
+    boolean chks[] = new boolean[4];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -116,6 +117,7 @@ public class MainActivity extends AppCompatActivity {
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle("多選項對話框測試");
+        b = chks.clone();
         builder.setMultiChoiceItems(R.array.drinks,b, new DialogInterface.OnMultiChoiceClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which, boolean isChecked) {
@@ -126,6 +128,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 StringBuilder sb = new StringBuilder();
+                chks = b.clone();
                 for (int i=0;i<=3;i++)
                 {
                     if (b[i])
@@ -140,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
         builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                b = chks.clone();
             }
         });
         builder.show();
